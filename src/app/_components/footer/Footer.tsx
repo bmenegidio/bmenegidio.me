389e04917c4f0296db1e02d10f98a1f8b2fd8af5
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-05 18:43:02 -0300

    refactor: keeps html more semantic

--- src/app/_components/footer/Footer.tsx
+++ src/app/_components/footer/Footer.tsx
@@ -2,14 +2,16 @@ import { Heading } from '@/components/heading/Heading';
 import { Content } from '@/components/surface/content/Content';
 
 export const Footer = () => (
-  <Content>
-    <div className={'p-10'}>
-      <Heading variant={'body'}>
-        Made with ❤️ by <strong>Bruno Menegidio</strong>
-      </Heading>
-      <Heading variant={'body'}>
-        &copy;{` `} {new Date().getFullYear()} Todos os direitos reservados.
-      </Heading>
-    </div>
-  </Content>
+  <footer>
+    <Content>
+      <div className={'p-10'}>
+        <Heading variant={'body'}>
+          Made with ❤️ by <strong>Bruno Menegidio</strong>
+        </Heading>
+        <Heading variant={'body'}>
+          &copy;{` `} {new Date().getFullYear()} Todos os direitos reservados.
+        </Heading>
+      </div>
+    </Content>
+  </footer>
 );
