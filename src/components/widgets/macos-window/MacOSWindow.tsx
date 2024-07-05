b35ffe0ddfc691a5da095e8794e76dbd1ec8a7b7
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-05 10:22:37 -0300

    feat: Create macOS Window Component

---
+++ src/components/widgets/macos-window/MacOSWindow.tsx
@@ -0,0 +1,16 @@
+import { MacOSWindowProps } from '@/components/widgets/macos-window/types/props';
+
+export const MacOSWindow = ({ children }: MacOSWindowProps) => (
+  <div className={'flex items-center justify-center'}>
+    <div className="overflow-hidden rounded-lg border border-gray-200 bg-white shadow-md">
+      <div className="flex items-center border-b border-gray-200 px-4 py-2">
+        <div className="flex space-x-1">
+          <span className="h-3 w-3 rounded-full bg-red-500"></span>
+          <span className="h-3 w-3 rounded-full bg-yellow-500"></span>
+          <span className="h-3 w-3 rounded-full bg-green-500"></span>
+        </div>
+      </div>
+      <div className={'bg-dracula-background font-code p-5'}>{children}</div>
+    </div>
+  </div>
+);
