82f809d82e5346be6a6c6f17c9c5808fd0a4ab4c
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-05 18:36:33 -0300

    feat: create caption variation

--- src/components/heading/types/props.ts
+++ src/components/heading/types/props.ts
@@ -2,5 +2,5 @@ import { ComponentProps, PropsWithChildren } from 'react';
 
 export type Props = Pick<ComponentProps<'h1'>, 'className'> &
   PropsWithChildren & {
-    variant: 'title' | 'subtitle' | 'body';
+    variant: 'title' | 'subtitle' | 'body' | 'caption';
   };
