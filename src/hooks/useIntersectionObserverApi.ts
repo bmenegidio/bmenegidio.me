26564f2e11083179419c6c8dd93c51556e81b213
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-05 16:46:36 -0300

    feat: create custom hook using intersection observer api

---
+++ src/hooks/useIntersectionObserverApi.ts
@@ -0,0 +1,31 @@
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
+
+export function useIntersectionObserverApi({
+  threshold,
+}: {
+  threshold: number;
+}): [MutableRefObject<HTMLDivElement | null>, boolean] {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return [ref, isVisible];
+}
