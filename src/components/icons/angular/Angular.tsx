d60a2fd306b6acd98422e4f9001e1e190a980469
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-05 15:51:34 -0300

    feat: create tech icons

---
+++ src/components/icons/angular/Angular.tsx
@@ -0,0 +1,10 @@
+export const AngularIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32">
+    <path fill="#DD0031" d="M16 2 3 7l2 17 11 6 11-6 2-17-13-5Z" />
+    <path fill="#C3002F" d="M16 2v28l11-6 2-17-13-5Z" />
+    <path
+      fill="#fff"
+      d="m16 5.094-8.127 18.27h3.03l1.634-4.088h6.898l1.634 4.088h3.03l-8.1-18.27Zm2.374 11.662h-4.748L16 11.03l2.374 5.726Z"
+    />
+  </svg>
+);
