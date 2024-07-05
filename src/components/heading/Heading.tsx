bede3f090fb18e0b85f4ca1c334f01121945c8b9
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-05 14:11:01 -0300

    style: fix font weight

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -4,10 +4,10 @@ import { Props } from '@/components/heading/types/props';
 
 export const Heading = ({ children, className, variant }: Props) => (
   <h1
-    className={clsx('leading-tight', className, {
-      'font-heading text-4xl font-bold': variant === 'title',
-      'font-heading text-3xl font-bold': variant === 'subtitle',
-      'font-body text-base': variant === 'body',
+    className={clsx('font-bold leading-tight', className, {
+      'font-heading text-4xl': variant === 'title',
+      'font-heading text-3xl': variant === 'subtitle',
+      'font-body text-base font-normal leading-5': variant === 'body',
     })}
   >
     {children}
