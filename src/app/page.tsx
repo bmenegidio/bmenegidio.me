1666654e8555b63242522e78104dc0b66c303603
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-18 17:31:42 -0300

    feat: use tech experience component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Experience } from '@/app/_components/experience/Experience';
 import { Footer } from '@/app/_components/footer/Footer';
 import { Header } from '@/app/_components/header/Header';
 import { Stack } from '@/app/_components/stack/Stack';
@@ -8,22 +9,24 @@ import { FadeInWhenVisible } from '@/components/animations/fade-in-when-visible/
 
 export default function Home() {
   return (
-    <div className={'w-screen'}>
-      <main>
-        <FadeInWhenVisible>
-          <Header />
-        </FadeInWhenVisible>
+    <main>
+      <FadeInWhenVisible>
+        <Header />
+      </FadeInWhenVisible>
 
-        <FadeInWhenVisible className={'relative z-10'}>
-          <WhoIam />
-        </FadeInWhenVisible>
+      <FadeInWhenVisible className={'relative z-10'}>
+        <WhoIam />
+      </FadeInWhenVisible>
 
-        <FadeInWhenVisible>
-          <Stack />
-        </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <Stack />
+      </FadeInWhenVisible>
 
-        <Footer />
-      </main>
-    </div>
+      <FadeInWhenVisible>
+        <Experience />
+      </FadeInWhenVisible>
+
+      <Footer />
+    </main>
   );
 }
