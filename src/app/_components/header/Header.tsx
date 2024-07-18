7d8e2a11ca43d1a17a071384f649dad5eb472c69
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-07-18 17:57:35 -0300

    ref: adjusted to use output -> export

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -6,16 +6,12 @@ import { MacOSWindow } from '@/components/widgets/macos-window/MacOSWindow';
 
 export const Header = () => (
   <header
-    className="mask-gradient-to-top relative flex flex-col gap-20 bg-cover bg-center p-5 pt-20"
+    className="relative flex flex-col gap-20 bg-cover bg-center p-5 pt-20 mask-gradient-to-top"
     style={{ backgroundImage: 'url(/space.png)' }}
   >
     <Content>
       <div className={'flex flex-col gap-2'}>
-        <Avatar
-          src={
-            'https://media.licdn.com/dms/image/D4D03AQHo5M4KFuAUXA/profile-displayphoto-shrink_400_400/0/1690417943208?e=1725494400&v=beta&t=Sq5gUiByaBkIFkiUY6QeKzUVoo1H_Y5wAYDJL8ObTDk'
-          }
-        />
+        <Avatar src={'/avatar.jpeg'} />
 
         <div className="max-w-[550px]">
           <Heading variant={'title'}>Desenvolvedor Web 🧑🏻‍💻❤️, entusiasta de Clean Code e Clean Architecture.</Heading>
